8d797d89def976268b426efe347f452711abeced
Mesha May Barcebas <bmeshamay@gmail.com>
2025-12-01 09:23:52 +0000

    Fix: use protected  property in User model (replace casts() method)

--- DocumentRequests/app/Models/User.php
+++ DocumentRequests/app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -35,17 +36,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string,string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_admin' => 'boolean',
+    ];
 
     /**
      * Get the document requests for the user.
